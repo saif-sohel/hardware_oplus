LineageOS/android_hardware_oplus

Add vendor.oplus.hardware.radio-V2 AIDL interface
Based on oplus-telephony-common.jar from qssi_64-user 15 AP3A.240617.008 1748422485332 release-keys

Change-Id: I90058c22ca1317967605690165efd0c5c4504fed

interfaces/aidl_api/vendor.oplus.hardware.radio/2/vendor/oplus/hardware/radio/IOplusCarrierIdentifierResponse.aidl
@@ -23,19 +23,7 @@
 
 package vendor.oplus.hardware.radio;
 @VintfStability
-interface IOplusRadioIndication {
-  void networkInfoInd(int type, in String[] networkInfo);
-  void oemHotswapProcessInd(int type, int process, int state);
-  void oemHotswapSetInd(int type, int state);
-  void oemKeyLogErrInd(int type, in vendor.oplus.hardware.radio.Oem_key_log_err_msg_type errMsg);
-  void oemLargeDataKeyLogErrInd(int type, in int[] errData);
-  void oemLogPacketInd(int type, in vendor.oplus.hardware.radio.Oem_log_packet_type logPacket);
-  void oemPinPukRetryNumInd(int type, int pin1, int pin2, int puk1);
-  void smlDeviceLockInfoChangedInd(int type, String lockInfo);
-  void smlEncryptedSerialIdInd(int type, in String[] serialIds);
-  void subsidyDeviceLockInfoChangedInd(int type, String lockInfo);
-  void regionlockStatusChangedInd(int type, in byte[] status);
-  void nrEvolutionTypeIndication(int type, int evolutionType);
-  void singleSimConfigChangedInd(int type, int config);
-  void offlineLockStatusChangedInd(int type, in int[] status);
+interface IOplusCarrierIdentifierResponse {
+  void setCallbackResponse(int result);
+  void setCarrierIdentifierIndEnableResponse(in vendor.oplus.hardware.radio.OplusRadioResponseInfo info);
 }

saif-sohel/hardware_oplus

interfaces/aidl_api/vendor.oplus.hardware.radio/2/vendor/oplus/hardware/radio/IOplusCarrierIdentifierResponse.aidl
@@ -22,19 +22,7 @@
 
 package vendor.oplus.hardware.radio;
 @VintfStability
-interface IOplusRadioIndication {
-  void networkInfoInd(int type, in String[] networkinfo);
-  void oemHotswapProcessInd(int type, int slotid, int error);
-  void oemHotswapSetInd(int type, int slotid);
-  void oemKeyLogErrInd(int type, in vendor.oplus.hardware.radio.Oem_key_log_err_msg_type errMsg);
-  void oemLargeDataKeyLogErrInd(int type, in int[] result);
-  void oemLogPacketInd(int type, in vendor.oplus.hardware.radio.Oem_log_packet_type log_packet_msg);
-  void oemPinPukRetryNumInd(int type, int slotid, int pin_retry_num, int puk_retry_num);
-  void smlDeviceLockInfoChangedInd(int type, String info);
-  void smlEncryptedSerialIdInd(int type, in String[] indStgs);
-  void subsidyDeviceLockInfoChangedInd(int type, String info);
-  void regionlockStatusChangedInd(int type, in byte[] region_lock_status_info);
-  void nrEvolutionTypeIndication(int type, int toe);
-  void singleSimConfigChangedInd(int type, int result);
-  void offlineLockStatusChangedInd(int type, in int[] status);
+interface IOplusCarrierIdentifierResponse {
+  void setCallbackResponse(int clientId);
+  void setCarrierIdentifierIndEnableResponse(in vendor.oplus.hardware.radio.OplusRadioResponseInfo info);
 }